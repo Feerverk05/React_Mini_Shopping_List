d55c1ec0646a30764e5ceb417113a24a8687ac28
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-16 21:53:33 +0200

    Update Form.jsx

--- shopping-list-react/src/component/Form.jsx
+++ shopping-list-react/src/component/Form.jsx
@@ -1,37 +1,47 @@
 import { useState } from "react";
 
-const Form = ({handleAddItems}) => {
+const Form = ({ handleAddItems }) => {
     const [product, setProducts] = useState("");
     const [quentity, setQuentity] = useState(1);
-    
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (product){
-        const newTodo = {
-            id: Date.now(),
-            packed: false,
-            product,
-            quentity,
-        };
-        console.log(newTodo);
-        handleAddItems(newTodo);
-        setProducts("");
-        setQuentity(1);
-    }
+        if (product) {
+            const newTodo = {
+                id: Date.now(),
+                packed: false,
+                product,
+                quentity,
+            };
+            handleAddItems(newTodo);
+            setProducts(""); 
+            setQuentity(1); 
+        }
     };
+
     return (
-    <form className="form" onSubmit={handleSubmit}>
-        <select value={quentity} onChange={(e) => setQuentity(Number(e.target.value))} className="select">
-        {[...Array(20)].map((_, index) => (
-            <option value={index + 1} key={index + 1}>
-                {index + 1}
-            </option>
-        ))}
-        </select>
-        <input className="input" type="text" placeholder="Enter a name" />
-        <button className="addBtn">Add</button>
-    </form>
+        <form className="form" onSubmit={handleSubmit}>
+            <select
+                value={quentity}
+                onChange={(e) => setQuentity(Number(e.target.value))}
+                className="select"
+            >
+                {[...Array(20)].map((_, index) => (
+                    <option value={index + 1} key={index + 1}>
+                        {index + 1}
+                    </option>
+                ))}
+            </select>
+            <input
+                className="input"
+                type="text"
+                placeholder="Enter a name"
+                value={product} 
+                onChange={(e) => setProducts(e.target.value)} 
+            />
+            <button className="addBtn">Add</button>
+        </form>
     );
 };
+
 export default Form;
