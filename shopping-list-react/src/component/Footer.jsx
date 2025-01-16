799a79f8f1ce757c784b2d56c55a982f78e51a5e
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-16 22:21:55 +0200

    Update Footer.jsx

--- shopping-list-react/src/component/Footer.jsx
+++ shopping-list-react/src/component/Footer.jsx
@@ -1,5 +1,13 @@
-const Footer = () => {
-    return <div className="footerWrapper"> Total Item: X 
-    Completed Item: X</div>;
+const Footer = ({items}) => {
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = (numPacked / numItems) * 100;
+    return (
+         <div className="footerWrapper">
+            { percentage === 100
+            ? "Shopping Done"
+            : `Total Item: ${items.length} ----- Completed Item: ${numPacked} --- ${percentage}%`
+            } </div>
+        );
 };
 export default Footer;
