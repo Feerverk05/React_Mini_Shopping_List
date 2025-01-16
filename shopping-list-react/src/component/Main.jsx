7fb7b1e6a050e569fe6c21e4adaeaf5dd786a261
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2025-01-16 22:02:33 +0200

    Update Main.jsx

--- shopping-list-react/src/component/Main.jsx
+++ shopping-list-react/src/component/Main.jsx
@@ -1,13 +1,13 @@
 import Form from "./Form";
 import List from "./List";
 
-const Main = ({items, handleAddItems, onDeleteItems}) => {
+const Main = ({items, handleAddItems, handleDoneItem, onDeleteItems}) => {
     return(
         <div>
             <Form handleAddItems={handleAddItems} />
             <ul className="mainWrapper">
             {items.map((item) => (
-                <List item={item} key={item.id} onDeleteItems={onDeleteItems}/>
+                <List item={item} key={item.id} onDeleteItems={onDeleteItems} handleDoneItem={handleDoneItem}/>
             ))}
         </ul>
         </div>
